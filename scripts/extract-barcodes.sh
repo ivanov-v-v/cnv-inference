98381e487a0991ed316ba07a4e776b2970ba23c2
ivanov-v-v <vy.vl.ivanov@gmail.com>
2019-07-17 20:34:20 +0300

    relative paths now supported; set a limit on the number of forks running simultaneously

--- scripts/extract-barcodes.sh
+++ scripts/extract-barcodes.sh
@@ -1,8 +1,14 @@
+#!/usr/bin/bash
+
 # This script takes .bam file as an input and extracts all unique cell barcodes present in that file.
 # It processes each chromosome separately (in parallel) and then merges the results. 
 # It also prints the chromosome name when done. Redirect the output to /dev/null to suppress it.
 
-tmp_dir=$1\-chr\-dir
+declare -i MAX_FORKS=8
+INFILE=$(echo $1 | sed 's/.*\///')
+echo "processing $INFILE"
+
+tmp_dir=$RANDOM\-chr\-dir
 mkdir $tmp_dir
 
 function chrom_from_bam() {
@@ -13,12 +19,19 @@ function cb_on_chrom() {
        samtools view $1 $2 | grep -Po "CB:Z:\K[A-Z]*(\-\d+){0,1}" | sort -uo "$tmp_dir/$2.txt"
 }
 
-chromosome_list=$(chrom_from_bam $1);
+chromosome_list=$(chrom_from_bam $1)
+declare -i forks_running=0
 for chrom in $chromosome_list; do
-       cb_on_chrom $1 $chrom & # here the parallelization happens
-       echo $chrom
+	echo $chrom 
+	forks_running+=1
+	if [ $forks_running -eq $MAX_FORKS ]; then 
+		wait
+		forks_running=0
+	fi
+	cb_on_chrom $INFILE $chrom & # here the parallelization happens
 done
 wait
 
-cat $tmp_dir/*.txt | sort --parallel 8 -uo $1_barcodes.txt
+cat $tmp_dir/*.txt | sort --parallel 8 -uo barcodes_$INFILE.txt
 rm -rf $tmp_dir	
+
