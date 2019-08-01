ab985365caeeb8c559a28f16b866ab0d2a5e6d35
viacheslav <ivanov.vv@phystech.edu>
2019-08-01 23:33:44 +0200

    add config file to the project

---
+++ config.py
@@ -0,0 +1,10 @@
+import os.path
+
+__all__ = ["ROOT_DIR", "LIB_DIR", "SRC_DIR", "DATA_DIR"]
+
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+LIB_DIR = os.path.join(ROOT_DIR, "lib")
+NOTEBOOKS_DIR = os.path.join(ROOT_DIR, "notebooks")
+SRC_DIR = os.path.join(ROOT_DIR, "src")
+DATA_DIR = os.path.join(ROOT_DIR, "data")
